5d11334bfdf2a03151f2c46dba879010b09ae9f5
Roshini <77736154+RoshiniVenkateswaran@users.noreply.github.com>
2025-04-09 13:46:07 -0400

    Minimium Operatioons to Make Array Values Equal to k

---
+++ 3375 - Easy.java
@@ -0,0 +1,33 @@
+//solution 1
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int min=nums[0];
+        if(min<k) return -1;
+
+        int c=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>k){
+                c++;
+                while(i+1<nums.length && nums[i]==nums[i+1])
+                  i++;
+            }
+        }
+        return c;
+    }
+}
+
+//solution 2
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        HashMap<Integer, Integer> mpp= new HashMap<>();
+
+        for(int n :nums){
+            if(n<k) return -1;
+            else if(n>k){
+                mpp.put(n, mpp.getOrDefault(n,0)+1);
+            }
+        }
+        return mpp.size();
+    }
+}
